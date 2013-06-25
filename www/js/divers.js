01fbfd7aef46a27ededeeeae71c93f17c932c35c
soufiane <s.barakat@linasolutions.ma>
2013-06-25 17:59:51 +0100

    §èuyhytyvbbn
    tytyutyubvnnb

--- www/js/divers.js
+++ www/js/divers.js
@@ -1,4 +1,7 @@
 function taco(var1) {
 	if(var1==1) document.getElementById('login_username').focus();
-	else if(var1==2)document.getElementById('login_passwd').focus();
+    else if(var1==3) document.getElementById('ti').value=1;
+     else if(var1==4) document.getElementById('ti').value=0;
+     else if(var1==2)document.getElementById('login_passwd').focus();
+    
 	}
