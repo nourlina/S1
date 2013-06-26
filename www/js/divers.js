e551a10ad3d76315f0fde3962c97bcc2237fdd12
soufiane <s.barakat@linasolutions.ma>
2013-06-26 18:53:32 +0100

    sss
    sss

--- www/js/divers.js
+++ www/js/divers.js
@@ -1,7 +1,9 @@
 function taco(var1) {
+
 	if(var1==1) document.getElementById('login_username').focus();
-    else if(var1==3) document.getElementById('ti').value=1;
-     else if(var1==4) document.getElementById('ti').value=0;
+     else if(var1==3) document.getElementById('ti').value=1;
+     else if(var1==4) document.getElementById('ti').value=4;
+	 else if(var1==5) document.getElementById('ti').value=0;
      else if(var1==2)document.getElementById('login_passwd').focus();
     
 	}
